a7743efcc3648ed2558d4abeddfa96d3d4aa939e
oibioib <pavelbodal@gmail.com>
2023-03-19 22:43:49 +0100

    feat: implement search component

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,44 @@
+import { Component } from 'react';
+
+import { STORAGE_KEYS, TEXT } from '@constants';
+
+import styles from './Search.module.scss';
+
+export interface SearchState {
+  searchValue: string;
+}
+
+class Search extends Component<Record<string, never>, SearchState> {
+  state = {
+    searchValue: localStorage.getItem(STORAGE_KEYS.SEARCH_VALUE) ?? '',
+  };
+
+  componentWillUnmount() {
+    localStorage.setItem(STORAGE_KEYS.SEARCH_VALUE, this.state.searchValue);
+  }
+
+  setSearchValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchValue: e.target.value });
+  };
+
+  render() {
+    const {
+      setSearchValue,
+      state: { searchValue },
+    } = this;
+
+    return (
+      <div className={styles.search}>
+        <input
+          className={styles.search__input}
+          type="text"
+          placeholder={TEXT.PLACEHOLDERS.SEARCH}
+          value={searchValue}
+          onChange={setSearchValue}
+        />
+      </div>
+    );
+  }
+}
+
+export default Search;
