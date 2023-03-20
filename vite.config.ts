27dbf6c68490e30d22c2e318650f7ba64cb4b0e0
oibioib <pavelbodal@gmail.com>
2023-03-20 23:29:20 +0100

    refactor: update configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,32 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
-import eslint from 'vite-plugin-eslint';
+import { configDefaults } from 'vitest/config';
 
 const folders = ['pages', 'components', 'layouts', 'constants', 'hoc', 'helpers'];
+
 const aliases = folders.map((folder) => ({
   find: `@${folder}`,
   replacement: path.resolve(__dirname, `./src/${folder}/index`),
 }));
 
+const panelStyle =
+  'background-color: #23272E; opacity: 0.95; min-height: 100%; top: 0px; transform: scale(1);';
+
+const lintCommand = 'eslint ./src --ext .ts,.tsx --ignore-path ./.gitignore';
+
 export default defineConfig({
   plugins: [
-    { ...react(), ...eslint(), apply: 'build' },
     {
       ...react(),
       ...checker({
         enableBuild: false,
         overlay: {
           position: 'tr',
-          panelStyle:
-            'background-color: #23272E; opacity: 0.95; min-height: 100%; top: 0px; transform: scale(1);',
+          panelStyle,
         },
         eslint: {
-          lintCommand: 'eslint ./src --ext .ts,.tsx --ignore-path ./.gitignore',
+          lintCommand,
         },
       }),
       apply: 'serve',
@@ -39,6 +43,7 @@ export default defineConfig({
       provider: 'c8',
       all: true,
       reporter: 'text',
+      exclude: [...configDefaults.coverage.exclude, '**/index.ts', '**/types.ts'],
     },
   },
   resolve: {
