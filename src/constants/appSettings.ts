772e0c3256e0dcd67224126ef3dac4c32f090c77
oibioib <pavelbodal@gmail.com>
2023-03-27 02:19:28 +0200

    feat: add app constants for form

--- src/constants/appSettings.ts
+++ src/constants/appSettings.ts
@@ -46,3 +46,42 @@ export const TEXT = {
     GO_MAIN: 'Go to main page!',
   },
 };
+
+export const ALERT = {
+  DURATION_MS: 2500,
+  MESSAGES: {
+    CARD_SUCCESS: 'Card added successfully!',
+  },
+  COLORS: {
+    SUCCESS: '#53b053',
+  },
+};
+
+export const FORM_ERROR_MESSAGE = {
+  TITLE: 'Please enter a name',
+  BRAND: 'Please select a brand from the list',
+  DATE: 'Please select a date greater than now',
+  CONDITION: 'Please select product condition',
+  FILE: 'Please select jpg or png file',
+};
+
+export const BRANDS = ['Samsung', 'Apple', 'Xiaomi', 'Huawei', 'Realme', 'Asus', 'Lenovo', 'LG'];
+
+export const AVAILABLE = {
+  TITLE: 'Available before',
+};
+
+export const CONDITION = {
+  TITLE: 'Condition',
+  NEW: 'new',
+  USED: 'used',
+};
+
+export const DELIVERY = {
+  TITLE: 'Delivery',
+  YES: 'available',
+  NO: 'unavailable',
+  IS_AVAILABLE: 'Delivery available?',
+};
+
+export const IMAGE_FORMATS = ['jpg', 'png'];
