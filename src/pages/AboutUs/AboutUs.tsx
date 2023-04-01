6568827706260e7c833e7a11329d621c3316f715
oibioib <pavelbodal@gmail.com>
2023-04-01 17:42:59 +0200

    feat: migrate to hooks AboutUs component

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,55 +1,49 @@
-import { Component } from 'react';
-
-class AboutUs extends Component {
-  render() {
-    return (
-      <div data-testid="about">
-        <p>Lorem ipsum</p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, ratione ut eos maxime
-          laboriosam debitis quia velit fugiat, consectetur et non esse aliquid asperiores vero
-          neque rem, excepturi quasi alias. Nisi amet, impedit odit voluptas dolor repellat
-          reprehenderit deserunt nihil numquam sequi quis quaerat enim, culpa nulla accusamus.
-          Obcaecati minus, libero in animi at quasi mollitia rerum eos ipsam, itaque reprehenderit
-          odio sit ipsa voluptatem totam ducimus nulla tenetur eveniet ullam deserunt porro
-          temporibus cum molestias! Quam corrupti rerum dolore.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, distinctio praesentium et
-          error esse nihil illo pariatur. Cumque, quam. Fugit reprehenderit perferendis tempore
-          consequatur. Quis, numquam deleniti voluptate quae ipsum illo maxime iusto praesentium
-          voluptatibus quos, veritatis, provident eligendi voluptas! Qui vero atque in.
-          Necessitatibus praesentium saepe earum sapiente nulla.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur fuga laudantium,
-          laborum cupiditate mollitia odio explicabo culpa placeat, excepturi recusandae accusantium
-          ipsum quidem aliquam nemo, laboriosam molestiae iste minima libero magni ab ratione!
-          Debitis in officia quidem officiis ratione quo dolorum atque fugit, blanditiis
-          reprehenderit autem. Nobis quod explicabo dolorem vero voluptatem quae amet quo, dicta
-          corporis esse. Eligendi dignissimos architecto debitis, officia molestias dolore error
-          neque a, aspernatur facere doloremque autem. Nostrum quo molestiae eius quam quaerat
-          doloribus consequuntur.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, itaque reprehenderit
-          deleniti aliquam amet ducimus quisquam ut odio nam mollitia, hic sunt nisi impedit odit
-          expedita perferendis, eius quibusdam architecto veniam ex iusto possimus? Cupiditate,
-          explicabo. Repellendus cum eius illo hic consectetur consequuntur vel, iste ullam vero
-          voluptate provident possimus architecto tenetur at dignissimos omnis ipsa delectus velit
-          quas eum harum minima perferendis rem. Non quo provident, illum repellendus nulla ratione
-          assumenda quis, accusantium distinctio maiores, possimus explicabo soluta qui asperiores
-          minima error suscipit libero. Sequi maxime sunt unde officiis quae deleniti eos tenetur
-          distinctio, reprehenderit nesciunt cumque perferendis repellat quos, accusantium
-          voluptatem dolor mollitia minus temporibus quaerat saepe earum? Et quasi dolorum eos
-          adipisci, blanditiis provident voluptatibus a earum non incidunt ratione? Aperiam et amet,
-          unde sequi similique deleniti nulla quia praesentium! Id provident facere voluptatibus
-          quae nisi saepe modi reiciendis aliquid tempora magnam architecto blanditiis odit, quos
-          repudiandae?
-        </p>
-      </div>
-    );
-  }
-}
+const AboutUs = () => {
+  return (
+    <div data-testid="about">
+      <p>Lorem ipsum</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, ratione ut eos maxime
+        laboriosam debitis quia velit fugiat, consectetur et non esse aliquid asperiores vero neque
+        rem, excepturi quasi alias. Nisi amet, impedit odit voluptas dolor repellat reprehenderit
+        deserunt nihil numquam sequi quis quaerat enim, culpa nulla accusamus. Obcaecati minus,
+        libero in animi at quasi mollitia rerum eos ipsam, itaque reprehenderit odio sit ipsa
+        voluptatem totam ducimus nulla tenetur eveniet ullam deserunt porro temporibus cum
+        molestias! Quam corrupti rerum dolore.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, distinctio praesentium et
+        error esse nihil illo pariatur. Cumque, quam. Fugit reprehenderit perferendis tempore
+        consequatur. Quis, numquam deleniti voluptate quae ipsum illo maxime iusto praesentium
+        voluptatibus quos, veritatis, provident eligendi voluptas! Qui vero atque in. Necessitatibus
+        praesentium saepe earum sapiente nulla.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur fuga laudantium,
+        laborum cupiditate mollitia odio explicabo culpa placeat, excepturi recusandae accusantium
+        ipsum quidem aliquam nemo, laboriosam molestiae iste minima libero magni ab ratione! Debitis
+        in officia quidem officiis ratione quo dolorum atque fugit, blanditiis reprehenderit autem.
+        Nobis quod explicabo dolorem vero voluptatem quae amet quo, dicta corporis esse. Eligendi
+        dignissimos architecto debitis, officia molestias dolore error neque a, aspernatur facere
+        doloremque autem. Nostrum quo molestiae eius quam quaerat doloribus consequuntur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, itaque reprehenderit deleniti
+        aliquam amet ducimus quisquam ut odio nam mollitia, hic sunt nisi impedit odit expedita
+        perferendis, eius quibusdam architecto veniam ex iusto possimus? Cupiditate, explicabo.
+        Repellendus cum eius illo hic consectetur consequuntur vel, iste ullam vero voluptate
+        provident possimus architecto tenetur at dignissimos omnis ipsa delectus velit quas eum
+        harum minima perferendis rem. Non quo provident, illum repellendus nulla ratione assumenda
+        quis, accusantium distinctio maiores, possimus explicabo soluta qui asperiores minima error
+        suscipit libero. Sequi maxime sunt unde officiis quae deleniti eos tenetur distinctio,
+        reprehenderit nesciunt cumque perferendis repellat quos, accusantium voluptatem dolor
+        mollitia minus temporibus quaerat saepe earum? Et quasi dolorum eos adipisci, blanditiis
+        provident voluptatibus a earum non incidunt ratione? Aperiam et amet, unde sequi similique
+        deleniti nulla quia praesentium! Id provident facere voluptatibus quae nisi saepe modi
+        reiciendis aliquid tempora magnam architecto blanditiis odit, quos repudiandae?
+      </p>
+    </div>
+  );
+};
 
 export default AboutUs;
