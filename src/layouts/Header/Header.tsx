65a43d3f0c87bb87097f587b911e83426c4d8903
oibioib <pavelbodal@gmail.com>
2023-04-01 17:41:37 +0200

    feat: migrate to hooks Header component

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,32 +1,26 @@
-import { Component } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { Menu } from '@components';
 import { MENU_LINKS } from '@constants';
 import { getPageTitle } from '@helpers';
-import { WithLocationProps, withLocation } from '@hoc';
 import base from '@scss/components/base.module.scss';
 
 import styles from './Header.module.scss';
 
-class Header extends Component<WithLocationProps, Record<string, never>> {
-  render() {
-    const {
-      location: { pathname },
-    } = this.props;
+const Header = () => {
+  const { pathname } = useLocation();
+  const pageTitle = getPageTitle(pathname, MENU_LINKS);
 
-    const pageTitle = getPageTitle(pathname, MENU_LINKS);
-
-    return (
-      <div className={styles.header}>
-        <div className={base.wrapper}>
-          <div className={styles.header__container}>
-            <h1 className={styles.header__title}>{pageTitle}</h1>
-            <Menu links={MENU_LINKS} />
-          </div>
+  return (
+    <div className={styles.header}>
+      <div className={base.wrapper}>
+        <div className={styles.header__container}>
+          <h1 className={styles.header__title}>{pageTitle}</h1>
+          <Menu links={MENU_LINKS} />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default withLocation(Header);
+export default Header;
