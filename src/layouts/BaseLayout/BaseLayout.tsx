bcc1d20547ba00d24c61a5a5cf7e02cb3712c759
oibioib <pavelbodal@gmail.com>
2023-04-01 17:46:12 +0200

    feat: migrate to hooks BaseLayout component

--- src/layouts/BaseLayout/BaseLayout.tsx
+++ src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,22 +1,17 @@
-import { Component } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Header } from '@layouts';
 import base from '@scss/components/base.module.scss';
 
-import styles from './BaseLayout.module.scss';
-
-class BaseLayout extends Component {
-  render() {
-    return (
-      <div className={styles.container}>
-        <Header />
-        <div className={base.wrapper}>
-          <Outlet />
-        </div>
+const BaseLayout = () => {
+  return (
+    <div>
+      <Header />
+      <div className={base.wrapper}>
+        <Outlet />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default BaseLayout;
