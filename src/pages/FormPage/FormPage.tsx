c2933293098c9dd59cc3b483115efbb50b73fcb0
oibioib <pavelbodal@gmail.com>
2023-04-01 18:03:21 +0200

    feat: migrate to hooks FormPage component

--- src/pages/FormPage/FormPage.tsx
+++ src/pages/FormPage/FormPage.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import { AddCardForm, Alert, Cards } from '@components';
 import { ALERT } from '@constants';
@@ -6,42 +6,30 @@ import { Card } from 'components/types';
 
 import styles from './FormPage.module.scss';
 
-interface FormPageState {
-  isAllert: boolean;
-  cards: Card[];
-}
+const FormPage = () => {
+  const [isAllert, setIsAllert] = useState<boolean>(false);
+  const [cards, setCards] = useState<Card[]>([]);
 
-class FormPage extends Component<Record<string, never>, FormPageState> {
-  constructor(props: Record<string, never>) {
-    super(props);
+  const onSubmit = (card: Card) => {
+    setCards((cards) => [...cards, card]);
+    setIsAllert(true);
 
-    this.state = {
-      isAllert: false,
-      cards: [],
-    };
-  }
-
-  onSubmit = (card: Card) => {
-    this.setState((state) => ({ cards: [...state.cards, card], isAllert: true }));
     setTimeout(() => {
-      this.setState({ isAllert: false });
+      setIsAllert(false);
     }, ALERT.DURATION_MS);
   };
 
-  render() {
-    const { isAllert, cards } = this.state;
-    return (
-      <>
-        {isAllert && (
-          <Alert message={ALERT.MESSAGES.CARD_SUCCESS} backgroundColor={ALERT.COLORS.SUCCESS} />
-        )}
-        <div className={styles.form_wrapper}>
-          <AddCardForm onSubmit={this.onSubmit} />
-        </div>
-        <Cards cards={cards} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {isAllert && (
+        <Alert message={ALERT.MESSAGES.CARD_SUCCESS} backgroundColor={ALERT.COLORS.SUCCESS} />
+      )}
+      <div className={styles.form_wrapper}>
+        <AddCardForm onSubmit={onSubmit} />
+      </div>
+      <Cards cards={cards} />
+    </>
+  );
+};
 
 export default FormPage;
