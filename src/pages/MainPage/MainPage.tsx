53515c2343476fcea2775ff643c6f8de0b2a17e9
oibioib <pavelbodal@gmail.com>
2023-04-01 17:46:51 +0200

    feat: migrate to hooks MainPage component and add context

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,18 +1,42 @@
-import { Component } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useBeforeUnload } from 'react-router-dom';
 
 import { Cards, Search } from '@components';
+import { STORAGE_KEYS, TEXT } from '@constants';
+import { AppContext } from '@context';
+import base from '@scss/components/base.module.scss';
 
 import cards from '../../data/cards.json';
 
-class MainPage extends Component {
-  render() {
-    return (
-      <>
-        <Search />
-        <Cards cards={cards} />
-      </>
-    );
-  }
-}
+const MainPage = () => {
+  const initSearchValue = localStorage.getItem(STORAGE_KEYS.SEARCH_VALUE) ?? '';
+  const [searchValue, setSearchValue] = useState<string>(initSearchValue);
+
+  const saveSearchValue = useCallback(() => {
+    localStorage.setItem(STORAGE_KEYS.SEARCH_VALUE, searchValue);
+  }, [searchValue]);
+
+  const cardsToRender = useMemo(
+    () =>
+      cards.filter(({ title, brand }) =>
+        [title, brand].some((field) =>
+          field.toLowerCase().includes(searchValue.toLowerCase().trim().replace(/[ ]+/g, ' '))
+        )
+      ),
+    [searchValue]
+  );
+
+  useEffect(() => saveSearchValue);
+  useBeforeUnload(saveSearchValue);
+
+  return (
+    <AppContext.Provider value={{ searchValue, setSearchValue }}>
+      <Search />
+      {(cardsToRender.length && <Cards cards={cardsToRender} />) || (
+        <p className={base.center}>{TEXT.MESSAGES.NO_CARDS_FOUNDED}</p>
+      )}
+    </AppContext.Provider>
+  );
+};
 
 export default MainPage;
