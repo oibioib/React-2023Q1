abf75fd93327a5fe7ac31a1b17bd1e44ba847083
oibioib <pavelbodal@gmail.com>
2023-04-01 17:45:37 +0200

    feat: migrate to hooks Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,45 +1,43 @@
-import { Component } from 'react';
+import { useContext } from 'react';
 
-import { STORAGE_KEYS, TEXT } from '@constants';
+import { TEXT } from '@constants';
+import { AppContext } from '@context';
 
 import styles from './Search.module.scss';
 
-export interface SearchState {
-  searchValue: string;
-}
+const Search = () => {
+  const { searchValue, setSearchValue } = useContext(AppContext);
 
-class Search extends Component<Record<string, never>, SearchState> {
-  state = {
-    searchValue: localStorage.getItem(STORAGE_KEYS.SEARCH_VALUE) ?? '',
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
   };
 
-  componentWillUnmount() {
-    localStorage.setItem(STORAGE_KEYS.SEARCH_VALUE, this.state.searchValue);
-  }
-
-  setSearchValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ searchValue: e.target.value });
+  const onClear = () => {
+    setSearchValue('');
   };
 
-  render() {
-    const {
-      setSearchValue,
-      state: { searchValue },
-    } = this;
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Escape') onClear();
+  };
 
-    return (
-      <div className={styles.search}>
-        <input
-          data-testid="search"
-          className={styles.search__input}
-          type="text"
-          placeholder={TEXT.PLACEHOLDERS.SEARCH}
-          value={searchValue}
-          onChange={setSearchValue}
-        />
-      </div>
-    );
-  }
-}
+  const inputStyles = searchValue
+    ? [styles.search__input, styles.search__input_close].join(' ')
+    : styles.search__input;
+
+  return (
+    <div className={styles.search}>
+      {searchValue && <span className={styles.search__clear} onClick={onClear} />}
+      <input
+        data-testid="search"
+        value={searchValue}
+        className={inputStyles}
+        type="text"
+        placeholder={TEXT.PLACEHOLDERS.SEARCH}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+      />
+    </div>
+  );
+};
 
 export default Search;
