bd34185a256a8892f2ada78456a1e838ccb7e699
oibioib <pavelbodal@gmail.com>
2023-04-09 22:43:48 +0200

    refactor: add prop to main context

--- src/context/MainPageContext/MainPageContext.ts
+++ src/context/MainPageContext/MainPageContext.ts
@@ -3,9 +3,11 @@ import { createContext } from 'react';
 interface MainPageContext {
   searchValue: string;
   setSearchValue: (searchValue: string) => void;
+  doSearch: (searchValue: string) => void;
 }
 
 export const MainPageContext = createContext<MainPageContext>({
   searchValue: '',
   setSearchValue: () => {},
+  doSearch: () => {},
 });
