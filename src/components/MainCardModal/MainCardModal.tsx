d5994363a6b810046ee0325d152d2df07337a804
oibioib <pavelbodal@gmail.com>
2023-04-10 23:58:15 +0200

    fix: change some styles

--- src/components/MainCardModal/MainCardModal.tsx
+++ src/components/MainCardModal/MainCardModal.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 
 import { getPhoto, preparePhoto } from '@api';
-import { Loader } from '@components';
 import { MainCardProps } from 'components/types';
 
 import styles from './MainCard.module.scss';
@@ -48,12 +47,16 @@ const MainCardModal = ({ id }: { id: string }) => {
 
   return (
     <>
-      {isLoading && (
-        <div className={styles['main-card-modal__loader']}>
-          <Loader />
-        </div>
-      )}
       <div className={styles['main-card-modal']}>
+        {isLoading && (
+          <>
+            <div className={styles['main-card-modal__avatar_loading']}></div>
+            <div className={styles['main-card-modal__author_loading']}></div>
+            <div className={styles['main-card-modal__date_loading']}></div>
+            <div className={styles['main-card-modal__description_loading']}></div>
+            <div className={styles['main-card-modal__image_loading']}></div>
+          </>
+        )}
         {!isLoading && mainCardModal && (
           <>
             <img className={styles['main-card-modal__avatar']} src={mainCardModal.authorAvatar} />
