29e30d9514b5fec3dc70595914a40ac3aab0935d
oibioib <pavelbodal@gmail.com>
2023-04-16 18:29:11 +0200

    refactor: change MainCardsModal component

--- src/components/MainCardModal/MainCardModal.tsx
+++ src/components/MainCardModal/MainCardModal.tsx
@@ -1,76 +1,45 @@
 import { useEffect, useState } from 'react';
 
-import { getPhoto, preparePhoto } from '@api';
-import { MainCardProps } from '@components/types';
+import { ErrorMessage, Loader } from '@components';
+import { formatDate, getErrorMessage, loadImage } from '@helpers';
+import { storeActions } from '@store';
 
 import styles from './MainCard.module.scss';
 
+const { useGetPhotoQuery } = storeActions.unsplashApi;
+
 const MainCardModal = ({ id }: { id: string }) => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [mainCardModal, setMainCardModal] = useState<MainCardProps | null>(null);
-  const [error, setError] = useState<Error | null>(null);
-
-  if (error) {
-    throw error;
-  }
-
-  const loadImage = (src: string) =>
-    new Promise((resolve) => {
-      const img = new Image();
-      img.src = src;
-      img.onload = () => resolve(true);
-    });
+  const { data, isFetching, isError, isLoading, error } = useGetPhotoQuery(id);
+  const [isImageLoaded, setIsImageLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     (async () => {
-      try {
-        setIsLoading(true);
-        const photo = await getPhoto(id);
-        const resultPhoto = preparePhoto(photo);
-        await loadImage(resultPhoto.img);
-        setMainCardModal(resultPhoto);
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error);
-        }
+      if (data) {
+        await loadImage(data.img);
+        setIsImageLoaded(true);
       }
-      setIsLoading(false);
     })();
-  }, [id]);
-
-  const formatDate = (date: string) => {
-    const data = new Date(date);
-    return `${data.getDate().toString().padStart(2, '0')}.${(data.getMonth() + 1)
-      .toString()
-      .padStart(2, '0')}.${data.getFullYear()} `;
-  };
+  }, [data]);
 
   return (
     <>
-      <div className={styles['main-card-modal']}>
-        {isLoading && (
-          <>
-            <div className={styles['main-card-modal__avatar_loading']}></div>
-            <div className={styles['main-card-modal__author_loading']}></div>
-            <div className={styles['main-card-modal__date_loading']}></div>
-            <div className={styles['main-card-modal__description_loading']}></div>
-            <div className={styles['main-card-modal__image_loading']}></div>
-          </>
-        )}
-        {!isLoading && mainCardModal && (
-          <>
-            <img className={styles['main-card-modal__avatar']} src={mainCardModal.authorAvatar} />
-            <span className={styles['main-card-modal__author']}>{mainCardModal.author}</span>
-            <span className={styles['main-card-modal__date']}>
-              {formatDate(mainCardModal.createdAt)}
-            </span>
-            <span className={styles['main-card-modal__description']}>
-              {mainCardModal.description}
-            </span>
-            <img className={styles['main-card-modal__image']} src={mainCardModal.img} />
-          </>
-        )}
-      </div>
+      {(isLoading || isFetching) && (
+        <div className={styles['main-card-modal__loader']}>
+          <Loader />
+        </div>
+      )}
+
+      {isError && <ErrorMessage message={getErrorMessage(error)} />}
+
+      {data && (
+        <div className={styles['main-card-modal']}>
+          <img className={styles['main-card-modal__avatar']} src={data.authorAvatar} />
+          <span className={styles['main-card-modal__author']}>{data.author}</span>
+          <span className={styles['main-card-modal__date']}>{formatDate(data.createdAt)}</span>
+          <span className={styles['main-card-modal__description']}>{data.description}</span>
+          {isImageLoaded && <img className={styles['main-card-modal__image']} src={data.img} />}
+        </div>
+      )}
     </>
   );
 };
